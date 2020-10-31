54383a12f2b11aa5197f37db80b0d6aa8253399b
escu-git <escuderopablo@hotmail.com.ar>
2020-10-31 03:06:53 -0300

    Cambio logo DarkMode

--- script/script.js
+++ script/script.js
@@ -1,18 +1,23 @@
 
-//INSERTIONS IN HTML
-    //Gifos img
-let mainLogo = document.querySelector("div.mainLogo");
-let logoDayMode =document.createElement("img");
-logoDayMode.src="assets/logo-mobile.svg";
-logoDayMode.alt="Logo Gifos";
-mainLogo.appendChild(logoDayMode);
-
-//DARK MODE:
-
+//Botón DARK/LIGHT MODE:
 const darkModeBtn = document.getElementById("darkMode");
 
+//Gifos IMG: Contenedor y creación de IMG con 2 src:
+let mainLogo = document.querySelector("div.mainLogo"); 
+let logoTheme =document.createElement("img");
+logoTheme.alt="Logo Gifos";
+logoTheme.src = "assets/logo-mobile.svg";
+let body = document.getElementById("body");
+mainLogo.appendChild(logoTheme);
+
+    
 darkModeBtn.addEventListener("click",(event)=>{
     let themeToggle = document.getElementById("body");
     themeToggle.classList.toggle("darkMode");
+    //Cambio de logo según theme
+    if(themeToggle.classList.contains("darkMode")){
+        logoTheme.src = "assets/logo-mobile-modo-noct.svg"
+    }else {
+        logoTheme.src = "assets/logo-mobile.svg"
+    }
 })
-
