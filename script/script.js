0b0439b065705bdfba0c551d3df73eaf94ab7fc4
escu-git <escuderopablo@hotmail.com.ar>
2020-11-07 16:41:39 -0300

    Funciones SEARCH

--- script/script.js
+++ script/script.js
@@ -25,4 +25,37 @@ darkModeBtn.addEventListener("click",(event)=>{
     }
 })
 
-//
+//TRENDING SECTION:
+
+//APIKEY for GIFOS: 5STmUZ3Fl2MXPNUrP5Rj8KfP5nAcf84u
+let divTrending = document.getElementById("trending");
+//INPUT SEARCH
+const inputSearch = document.getElementById("search");
+
+
+inputSearch.addEventListener("keyup", (event)=>{
+    if(event.keyCode === 13){
+        buscarGifs(inputSearch.value)
+    }
+});
+//BARRA DE BÚSQUEDA: 
+let buscarGifs = (valorDeInput)=> {
+let searchGifs = `https://api.giphy.com/v1/gifs/search?api_key=5STmUZ3Fl2MXPNUrP5Rj8KfP5nAcf84u&q=${valorDeInput}&limit=25&offset=0&rating=g&lang=en`;
+
+fetch(searchGifs)
+.then(response=>response.json())
+.then(response => console.log(response))
+
+.catch(error =>{
+    alert("No se encontró nada")
+
+    return searchGifs
+})
+}
+// let imprimirGifs= (buscarGifs()) =>{
+//     alert("Hola")
+// }
+    
+// })
+
+
