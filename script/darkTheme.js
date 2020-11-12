f883db0a06d7fe50db0cff2e54d69e9fea9f575a
Pablo Martín Escudero <escuderopablo@hotmail.com.ar>
2020-11-12 13:18:49 -0300

    update3

---
+++ script/darkTheme.js
@@ -0,0 +1,25 @@
+//Botón DARK/LIGHT MODE:
+const darkModeBtn = document.getElementById("darkMode");
+
+//Gifos IMG: Contenedor y creación de IMG con 2 src:
+let mainLogo = document.querySelector("div.mainLogo"); 
+let logoTheme =document.createElement("img");
+logoTheme.alt="Logo Gifos";
+logoTheme.src = "assets/logo-mobile.svg";
+let body = document.getElementById("body");
+mainLogo.appendChild(logoTheme);
+
+darkModeBtn.addEventListener("click",(event)=>{
+    let themeToggle = document.getElementById("body");
+    themeToggle.classList.toggle("darkMode");
+    //Cambio de logo según theme
+    let diurnoLi = document.getElementById("darkMode");
+    if(themeToggle.classList.contains("darkMode")){
+        logoTheme.src = "assets/logo-mobile-modo-noct.svg";
+        diurnoLi.innerHTML = "Modo Diurno";
+
+    }else {
+        logoTheme.src = "assets/logo-mobile.svg";
+        diurnoLi.innerHTML = "Modo Nocturno";
+    }
+})
---
+++ script/script.js
@@ -0,0 +1,37 @@
+//SEARCH SECTION:
+const divSearch = document.getElementById("searchContainer");
+const inputSearch = document.getElementById("search");
+
+//CREATE ELEMENTS:
+let newDiv = document.createElement('div');
+let newGif = document.createElement('img');
+
+inputSearch.addEventListener("keyup", (event)=>{
+    if(event.keyCode === 13){ //Activa búsqueda con enter.
+        printSearch(buscarGifs());//Busca en API e imprime en DOM.
+        inputSearch.value = ""; //Vaciar casilla de búsqueda.
+    }
+});
+
+//////////////SEARCH GIFS FUNCTION
+async function buscarGifs(valorDeInput){
+    let urlSearch = `https://api.giphy.com/v1/gifs/search?api_key=5STmUZ3Fl2MXPNUrP5Rj8KfP5nAcf84u&q=${valorDeInput}&limit=25&offset=0&rating=g&lang=en`;
+
+    let response = await fetch(urlSearch);
+    return response.json();
+}
+//////////////PRINT CONTENT IN DOM FUNCTION
+let imprimirDOM= (imagen)=>{
+    newGif.src = imagen;
+    divSearch.appendChild(newDiv.appendChild(newGif));
+}
+async function printSearch(fnBuscar) {
+    let printSearchArray = []; //Array con contenido de API
+    let apiArray = await fnBuscar
+    apiArray.data.forEach(array=>{
+        printSearchArray.push(array);
+    })
+    printSearchArray.forEach(imagenes=>{
+        imprimirDOM(imagenes.images.downsized.url)
+    })
+};
