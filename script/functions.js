2f07c2dc34dfe03d29df307b071770d6a6a26500
Pablo Martín Escudero <escuderopablo@hotmail.com.ar>
2020-11-12 12:55:50 -0300

    Añadidas funciones Search

--- script/functions.js
+++ script/functions.js
@@ -3,21 +3,3 @@
 //////////////TRENDING
 
 
-
-
-
-
-
-
-
-//////////////SEARCH GIFS
-let buscarGifs = (valorDeInput)=> {
-    let searchGifs = `https://api.giphy.com/v1/gifs/search?api_key=5STmUZ3Fl2MXPNUrP5Rj8KfP5nAcf84u&q=${valorDeInput}&limit=25&offset=0&rating=g&lang=en`;
-    
-    fetch(searchGifs)
-    .then(response=>response.json())
-    .then(array => console.log(array))
-    .catch(error =>{
-        alert("No se encontró nada")
-    })
-}
